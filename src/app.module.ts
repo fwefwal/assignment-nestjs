5a92c4301028f249426154da159cd4b869e6890c
Lex Trofimov <daslef93@gmail.com>
2025-03-23 00:37:02 +0300

    generate resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
 import { TagsModule } from './tags/tags.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ArticlesModule } from './articles/articles.module';
 
 @Module({
-  imports: [TagsModule, PrismaModule],
+  imports: [TagsModule, ArticlesModule, PrismaModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
