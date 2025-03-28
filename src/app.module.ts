91747a0d19e01c3614f9a8c17c427903b2efd938
Lex Trofimov <daslef93@gmail.com>
2025-03-28 09:17:57 +0300

    fix: remove auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common'
 import { TagsModule } from './tags/tags.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ArticlesModule } from './articles/articles.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [TagsModule, ArticlesModule, PrismaModule, AuthModule],
