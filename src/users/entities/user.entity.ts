970572b01e462ee2a1bf03d8ce2b339cb7aaf383
Lex Trofimov <daslef93@gmail.com>
2025-03-28 10:56:04 +0300

    WIP: users

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,30 @@
-export class User {}
+import { ApiProperty } from '@nestjs/swagger'
+import { User } from '@prisma/client'
+import { Exclude } from 'class-transformer'
+
+export class UserEntity {
+  @ApiProperty()
+  email: string
+
+  @ApiProperty()
+  token: string | undefined
+
+  @ApiProperty()
+  username: string
+
+  @ApiProperty()
+  bio: string
+
+  @ApiProperty()
+  image: string | null
+
+  @Exclude()
+  password: string
+
+  @Exclude()
+  id: number
+
+  constructor(userData: Partial<User>) {
+    Object.assign(this, userData)
+  }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,34 +1,45 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common'
+import { UsersService } from './users.service'
+import { CreateUserDto } from './dto/create-user.dto'
+import { UpdateUserDto } from './dto/update-user.dto'
+import { UserEntity } from './entities/user.entity'
+import { ApiCreatedResponse } from '@nestjs/swagger'
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiCreatedResponse({ type: UserEntity })
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  async create(@Body() createUserDto: CreateUserDto) {
+    return new UserEntity(await this.usersService.create(createUserDto))
   }
 
   @Get()
   findAll() {
-    return this.usersService.findAll();
+    return this.usersService.findAll()
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+    return this.usersService.findOne(+id)
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+    return this.usersService.update(+id, updateUserDto)
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+    return this.usersService.remove(+id)
   }
 }
